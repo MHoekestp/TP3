1786875aa20d67cb191589869ec37f8ec898c9c4
pedago <pedago@pcalb-mm0608.univ-jfc.fr>
2019-09-24 15:06:26 +0200

    TP3 debut

--- src/main/java/bowling/SinglePlayerGame.java
+++ src/main/java/bowling/SinglePlayerGame.java
@@ -1,5 +1,7 @@
 package bowling;
 
+import java.util.ArrayList;
+
 /**
  * Cette classe a pour but d'enregistrer le nombre de quilles abattues lors des
  * lancés successifs d'<b>un seul et même</b> joueur, et de calculer le score
@@ -12,6 +14,11 @@ public class SinglePlayerGame {
 	 */
 	public SinglePlayerGame() {
 	}
+        public int lances=0;
+        public int tours=0;
+        public ArrayList playerScore= new ArrayList<Integer>();
+        public boolean lastTurnStrike = false;
+
 
 	/**
 	 * Cette méthode doit être appelée à chaque lancé de boule
@@ -20,15 +27,29 @@ public class SinglePlayerGame {
 	 * ce lancé
 	 */
 	public void lancer(int nombreDeQuillesAbattues) {
-
+            this.playerScore.add(lances,nombreDeQuillesAbattues);
+                this.lances+=1;                      
 	}
-
+    
 	/**
 	 * Cette méthode donne le score du joueur
 	 *
 	 * @return Le score du joueur
 	 */
 	public int score() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+                return sum(playerScore);
 	}
+        public int sum(ArrayList<Integer> score){
+            int sum = 0;
+            for(int i = 0; i < score.size(); i++){
+                if(score.get(i)==10){
+                    sum = sum+score.get(i)+score.get(i+1)+score.get(i+2);
+                }
+                else{
+                  sum = sum+score.get(i);  
+                }
+                
+            }
+            return sum;
+        }
 }
